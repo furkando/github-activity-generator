492195f26424dc82a78e1f066e3a1fba55bf035e
Furkan Dogan <furkandoganktf@hotmail.com>
2022-03-07 23:40:16 +0300

    handle commits

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import { series } from "./shellHelper.js";
 import moment from "moment";
 
-let textSwitch = 0;
+let textSwitch = true;
 
+// Mon Mar 7 23:29:49 2022 +0300 => Git date format
 const commit = (date) => {
+  const dateString = date.format("ddd MMM D HH:mm:ss Y +0300");
+  textSwitch = !textSwitch;
   series([
     `echo "${textSwitch}" > commit.txt`,
     "git add -A",
-    "git commit --verbose",
+    `git commit -m "Old commit"`,
     `GIT_COMMITTER_DATE="${dateString}" git commit --amend --date="${dateString}" --no-edit`,
   ]);
 };
@@ -19,6 +22,7 @@ const generateActivity = (startDate, endDate) => {
     m.add(1, "days")
   ) {
     console.log(`Generating activity for ${m.format("YYYY-MM-DD")}`);
+    commit(m);
   }
 };
 
